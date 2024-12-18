424c34d47351512e73e7e370a3846427f2b1eac0
Olivier Roberge <31775707+oliovio@users.noreply.github.com>
2024-12-18 16:32:23 -0500

    Work in progress

--- Frontend/src/assets/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white text-center p-4">
-    <p>© {new Date().getFullYear()} PropEase. All Rights Reserved.</p>
-  </footer>
-);
-
-export default Footer;
